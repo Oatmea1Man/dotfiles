74a288609972293ede774380bce53c0fc35dd00b
Chris <celeach423@gmail.com>
2025-06-20 13:22:21 -0400

    working init.lua before moving plugins to files

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,3 +5,60 @@ vim.cmd("set shiftwidth=4")
 
 vim.cmd("set number")
 vim.cmd("set relativenumber")
+
+vim.keymap.set('i', 'jk', '<Esc>')
+
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out, "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.g.mapleader = " "       -- set <leader> as <space>
+vim.g.maplocalleader = " "  -- also set <localleader> as <space>
+
+local plugins = {
+    { 'catppuccin/nvim', name = 'catppuccin', priority = 1000 },
+    {
+        'nvim-telescope/telescope.nvim', tag = '0.1.5',
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    { 'nvim-treesitter/nvim-treesitter', build = ":TSUpdate" },
+    {
+        'nvim-neo-tree/neo-tree.nvim',
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons",
+            "MunifTanjim/nui.nvim",
+        }
+    },
+}
+local opts = {}
+
+require("lazy").setup(plugins, opts)
+local builtin = require("telescope.builtin")
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
+vim.keymap.set('n', '<C-n>', ':Neotree filesystem toggle left<CR>')
+
+local config = require("nvim-treesitter.configs")
+config.setup({
+    ensure_installed = {"lua", "python", "javascript"},
+    highlight = { enable = true },
+    indent = { disable = { 'lua' } },
+})
+require("catppuccin").setup()
+vim.cmd.colorscheme 'catppuccin'
