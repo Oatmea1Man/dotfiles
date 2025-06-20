a292b024d1fbe5d2a159cef12aa3c024ec5ea013
Chris <celeach423@gmail.com>
2025-06-20 14:31:55 -0400

    put each plugin in its own config file

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,13 +1,3 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=4")
-vim.cmd("set softtabstop=4")
-vim.cmd("set shiftwidth=4")
-
-vim.cmd("set number")
-vim.cmd("set relativenumber")
-
-vim.keymap.set('i', 'jk', '<Esc>')
-
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -25,40 +15,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-vim.g.mapleader = " "       -- set <leader> as <space>
-vim.g.maplocalleader = " "  -- also set <localleader> as <space>
-
-local plugins = {
-    { 'catppuccin/nvim', name = 'catppuccin', priority = 1000 },
-    {
-        'nvim-telescope/telescope.nvim', tag = '0.1.5',
-        dependencies = { 'nvim-lua/plenary.nvim' }
-    },
-    { 'nvim-treesitter/nvim-treesitter', build = ":TSUpdate" },
-    {
-        'nvim-neo-tree/neo-tree.nvim',
-        branch = "v3.x",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "nvim-tree/nvim-web-devicons",
-            "MunifTanjim/nui.nvim",
-        }
-    },
-}
 local opts = {}
 
-require("lazy").setup(plugins, opts)
-local builtin = require("telescope.builtin")
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-
-vim.keymap.set('n', '<C-n>', ':Neotree filesystem toggle left<CR>')
-
-local config = require("nvim-treesitter.configs")
-config.setup({
-    ensure_installed = {"lua", "python", "javascript"},
-    highlight = { enable = true },
-    indent = { disable = { 'lua' } },
-})
-require("catppuccin").setup()
-vim.cmd.colorscheme 'catppuccin'
+require("vim-options")
+require("lazy").setup("plugins")
---
+++ nvim/lua/vim-options.lua
@@ -0,0 +1,13 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+
+vim.cmd("set number")
+vim.cmd("set relativenumber")
+
+vim.keymap.set('i', 'jk', '<Esc>')
+
+vim.g.mapleader = " "       -- set <leader> as <space>
+vim.g.maplocalleader = " "  -- also set <localleader> as <space>
+
