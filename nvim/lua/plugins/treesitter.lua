be76609762e9d343603a8a7a70ee1cfd5df8cfb3
Chris <celeach423@gmail.com>
2025-06-28 18:06:54 -0400

    nvim config updated with lsp integration

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,7 +2,7 @@ return { 'nvim-treesitter/nvim-treesitter', build = ":TSUpdate",
 config = function()
 local config = require("nvim-treesitter.configs")
 config.setup({
-    ensure_installed = {"lua", "python", "javascript"},
+    ensure_installed = {"lua", "python", "javascript", "html"},
     highlight = { enable = true },
     indent = { disable = { 'lua' } },
 })
