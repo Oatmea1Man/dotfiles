f4d0bc29a25e8d13d88f634720afcb5dd0776f69
OatmealMan <oatmealman@mail.local>
2025-07-02 05:36:30 -0400

    finished initial configuration for language servers, none-ls

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,10 +1,13 @@
-return { 'nvim-treesitter/nvim-treesitter', build = ":TSUpdate",
-config = function()
-local config = require("nvim-treesitter.configs")
-config.setup({
-    ensure_installed = {"lua", "python", "javascript", "html"},
-    highlight = { enable = true },
-    indent = { disable = { 'lua' } },
-})
-end
+return {
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local config = require("nvim-treesitter.configs")
+		config.setup({
+			auto_install = true,
+			--ensure_installed = {"lua", "python", "javascript", "html"},
+			highlight = { enable = true },
+			indent = { disable = { "lua" } },
+		})
+	end,
 }
